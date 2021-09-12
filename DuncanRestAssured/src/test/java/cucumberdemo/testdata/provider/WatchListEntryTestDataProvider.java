145b9b3a03bf2a5b6b220e1b68f45aa1bf832663
Michael Duncan <mwduncan2018@gmail.com>
2021-09-12 16:49:17 -0400

    Update test data provider

--- DuncanRestAssured/src/test/java/cucumberdemo/testdata/provider/WatchListEntryTestDataProvider.java
+++ DuncanRestAssured/src/test/java/cucumberdemo/testdata/provider/WatchListEntryTestDataProvider.java
@@ -2,75 +2,95 @@ package cucumberdemo.testdata.provider;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import cucumberdemo.configuration.ConfigurationProperties;
-import cucumberdemo.testdata.model.post.PostRoot;
-import cucumberdemo.testdata.model.post.PostWatchListEntry;
+import cucumberdemo.restapi.model.watchlistentry.WatchListEntry;
 
-public class PostWatchListEntryProvider {
-	private static String jsonTestDataPath = ConfigurationProperties.getTestDataPath() + "/post-watchlist-entry.json";
+public class WatchListEntryTestDataProvider {
+	private static String jsonTestDataPath = ConfigurationProperties.getTestDataPath() + "/watchlist-entries.json";
 
-	public static List<PostWatchListEntry> getAll() {
-		PostRoot root = unmarshalJson();
-		return root.getPostWatchListEntries();
+	// Returning test data should not be in the same file as converting JSON to object.
+	// Make a new class for conversion between JSON and object.
+	
+	// Returns all test data
+	// Returns a List<WatchListEntry> 
+	public static List<WatchListEntry> getAll() {
+		return Arrays.asList(unmarshalJson());
 	}
 
-	public static PostWatchListEntry getByName(String firstName, String lastName) {
-		PostRoot root = unmarshalJson();
-		for (PostWatchListEntry x : root.getPostWatchListEntries()) {
+	// Returns test data
+	// Returns a WatchListEntry by first name and last name
+	public static WatchListEntry getByName(String firstName, String lastName) {
+		for (WatchListEntry x : getAll()) {
 			if (x.getFirstName().equals(firstName) && x.getLastName().equals(lastName)) {
 				return x;
 			}
 		}
-		return null;		
+		return null;
 	}
 
-	public static String marshalJson(PostWatchListEntry postWatchListEntry) {
+	// Converts one WatchListEntry to JSON
+	public static String marshalJson(WatchListEntry watchListEntry) {
 		try {
-			return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(postWatchListEntry);
+			return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(watchListEntry);
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 
-	public static String marshalJson(List<PostWatchListEntry> postWatchListEntries) {
+	// Converts an array of WatchListEntry to JSON
+	public static String marshalJson(WatchListEntry[] watchListEntryArray) {
 		try {
-			return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(postWatchListEntries);
+			return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(watchListEntryArray);
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 
-	public static String marshalJson(PostRoot root) {
+	// Converts a list of WatchListEntry to JSON
+	public static String marshalJson(List<WatchListEntry> watchListEntries) {
 		try {
-			return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(root);
+			return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(watchListEntries);
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 
-	
-	
-	private static PostRoot unmarshalJson() {
-		PostRoot root = null;
+	// Converts JSON to an array of WatchListEntry
+	public static WatchListEntry[] unmarshalJson(String json) {
+		WatchListEntry[] watchListEntryArray = null;
 		try {
 			ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
 					false);
-			File file = new File(jsonTestDataPath);
-			root = objectMapper.readValue(file, PostRoot.class);
+			watchListEntryArray = objectMapper.readValue(json, WatchListEntry[].class);
 		} catch (IOException e) {
 			System.out.println("An exception occurred while extracting JSON test data");
 			e.printStackTrace();
 		}
-		return root;
+		return watchListEntryArray;
+	}
+
+	// Reads a file and converts the JSON to an array of WatchListEntry
+	private static WatchListEntry[] unmarshalJson() {
+		WatchListEntry[] watchListEntryArray = null;
+		try {
+			ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+					false);
+			File file = new File(jsonTestDataPath);
+			watchListEntryArray = objectMapper.readValue(file, WatchListEntry[].class);
+		} catch (IOException e) {
+			System.out.println("An exception occurred while extracting JSON test data");
+			e.printStackTrace();
+		}
+		return watchListEntryArray;
 	}
 }
